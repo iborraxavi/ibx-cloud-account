4a417b52dad9ae46bded529b03da91bbeef34ef8
Xavi Iborra <xavi.iborra.95@gmail.com>
2023-12-05 12:56:53 +0100

    Add discovery annotation

--- application/src/main/java/com/ibx/account/application/AccountApplication.java
+++ application/src/main/java/com/ibx/account/application/AccountApplication.java
@@ -2,12 +2,14 @@ package com.ibx.account.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AccountApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
 
 }
